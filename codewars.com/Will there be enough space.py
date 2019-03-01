d9668b722f337f407a16d769e068b546cc71b19f
aseldyukov <32552393+aseldyukov@users.noreply.github.com>
2019-03-01 20:13:48 +0200

    Update Will there be enough space.py

--- codewars.com/Will there be enough space.py
+++ codewars.com/Will there be enough space.py
@@ -1,4 +1,12 @@
 # Will there be enough space?
 
+# Bob is working as a bus driver. However, he has become extremely popular amongst the city's residents. 
+# With so many passengers wanting to get aboard his bus, 
+# he sometimes has to face the problem of not enough space left on the bus! 
+# He wants you to write a simple program telling him if he will be able to fit all the passengers.
+# Task Overview:
+# You have to write a function that accepts three parameters:
+# If there is enough space, return 0, and if there isn't, return the number of passengers he can't take.
+
 def enough(cap, on, wait):
     return abs(cap-(on+wait))
